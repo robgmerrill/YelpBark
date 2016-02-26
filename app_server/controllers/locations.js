cdfd7099ebd37751fc20163073b296e0817a8edb
Rob Merrill <robgmerrill@gmail.com>
2016-02-26 07:23:13 -0800

    finished static prototype for about/views/locations/reviews

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET home page */
 module.exports.homelist = function(req, res) {
-  res.render('locations-list', { title: 'Home' });
+  res.render('locations-list', { 
+    title: 'YelpBark - find great places to bring your four legged friends!', 
+    pageHeader: {
+      title: 'YelpBark',
+      strapline: 'Find great places to bring your four legged friends!'
+    },
+    sidebar: "Looking for a place to bring your four legged friend? We have found the place for you! Join in and help others find great places for their four legged friends!",
+    locations: [{
+      name: 'Fremont Brewery',
+      address: '125 High Street, Reading, RG6 1PS', 
+      rating: 5,
+      facilities: ['Indoor', 'Outdoor', 'Water Bowl', 'Treats'],
+      distance: '100m'
+    }, {
+      name: 'Seward Park',
+      address: '125 High Street, Reading, RG6 1PS', 
+      rating: 5,
+      facilities: ['Outdoor', 'Walk'],
+      distance: '100m'
+    }, {
+      name: 'Marymoore Park',
+      address: '125 High Street, Reading, RG6 1PS', 
+      rating: 5,
+      facilities: ['Outdoor', 'Walk', 'Dog Park'],
+      distance: '100m'
+    }]
+  });
 };
 
 /* GET location info page */
