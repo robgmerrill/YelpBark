681224bbd024f73fd349fad97ba8ceb9acd3a42a
Rob Merrill <robgmerrill@gmail.com>
2016-03-02 07:12:15 -0800

    reviews route fixed

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,7 +6,8 @@ var ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
-router.get('/location/review/new', ctrlLocations.addReview);
+router.get('/location/:locationid/review/new', ctrlLocations.addReview);
+router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
 
 // Other pages
 router.get('/about', ctrlOthers.about);
