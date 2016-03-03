fb87f9342797a8cadba9d0ad01d008eff8df706d
Rob Merrill <robgmerrill@gmail.com>
2016-03-03 13:48:10 -0800

    increased radius for mobile search

--- app_client/common/services/yelpbarkData.service.js
+++ app_client/common/services/yelpbarkData.service.js
@@ -6,7 +6,7 @@ angular
 
 function yelpbarkData ($http) {
   var locationByCoords = function (lat, lng) {
-    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=2000000000000000000000000000000000000000000000000000');
+    return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=2000000000000000000000000000000000000000000000000000000000000000000000000000');
   };
   return {
     locationByCoords : locationByCoords
