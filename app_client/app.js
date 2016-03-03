e17c50c682824d5297904f6dd0c53456f939ad31
Rob Merrill <robgmerrill@gmail.com>
2016-03-03 07:30:58 -0800

    added ngRoute

--- app_client/app.js
+++ app_client/app.js
@@ -1,1 +1,13 @@
-angular.module('yelpbarkApp', []);
+angular.module('yelpbarkApp', ['ngRoute']);
+
+function config($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'home/home.view.html'
+    })
+    .otherwise({redirectTo: '/'});
+}
+
+angular
+  .module('yelpbarkApp')
+  .config(['$routeProvider', config])
